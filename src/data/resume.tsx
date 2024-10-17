6c5ceee42993b8def4ee3301a4bfdd48b3b18c69
Michael Tarimo, MD, Dev <56628755+Michaeltarimo@users.noreply.github.com>
2024-10-17 16:12:30 +0300

    push resume update

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -193,64 +193,64 @@ export const DATA = {
   ],
   projects: [
     {
-      title: "Chat Collect",
-      href: "https://chatcollect.com",
-      dates: "Jan 2024 - Feb 2024",
+      title: "Automated Research Co-pilot",
+      href: "https://rezour.com",
+      dates: "Aug 2024 - Dec 2024",
       active: true,
       description:
-        "With the release of the [OpenAI GPT Store](https://openai.com/blog/introducing-the-gpt-store), I decided to build a SaaS which allows users to collect email addresses from their GPT users. This is a great way to build an audience and monetize your GPT API usage.",
+        "With recent AI advancements, I'm building Rezour, an automated SaaS platform that streamlines clinical study management. Using [OpenAI GPT](https://openai.com/), Rezour simplifies data collection and enhances collaboration among healthcare professionals.",
       technologies: [
         "Next.js",
-        "Typescript",
-        "PostgreSQL",
+        "JavaScript",
+        "PostgreSQL (Neon)",
         "Prisma",
         "TailwindCSS",
-        "Stripe",
         "Shadcn UI",
-        "Magic UI",
+        "Clerk Auth",
+        "OpenAI API"
       ],
       links: [
         {
           type: "Website",
-          href: "https://chatcollect.com",
-          icon: <Icons.globe className="size-3" />,
-        },
-      ],
-      image: "",
-      video:
-        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/chat-collect.mp4",
-    },
-    {
-      title: "Magic UI",
-      href: "https://magicui.design",
-      dates: "June 2023 - Present",
-      active: true,
-      description:
-        "Designed, developed and sold animated UI components for developers.",
-      technologies: [
-        "Next.js",
-        "Typescript",
-        "PostgreSQL",
-        "Prisma",
-        "TailwindCSS",
-        "Stripe",
-        "Shadcn UI",
-        "Magic UI",
-      ],
-      links: [
-        {
-          type: "Website",
-          href: "https://magicui.design",
+          href: "https://rezour.com",
           icon: <Icons.globe className="size-3" />,
         },
         {
           type: "Source",
-          href: "https://github.com/magicuidesign/magicui",
+          href: "https://github.com/rezourlab",
           icon: <Icons.github className="size-3" />,
         },
       ],
       image: "",
-      video: "https://cdn.magicui.design/bento-grid.mp4",
+      video:
+        "/vids/tanapa_demo.mp4",
+    },
+    {
+      title: "Tanapa UI",
+      href: "https://tanapa.vercel.app",
+      dates: "March - April 2024",
+      active: true,
+      description:
+        "Developed a fully functional website from a well-prepared UI for TANAPA. Delivered a responsive and interactive user experience with optimized performance.",
+      technologies: [
+        "Next.js",
+        "TailwindCSS",
+        "DaisyUI",
+        "Vercel",
+        "JavaScript",
+        "React Query",
+        "ESLint",
+        "Prettier",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://tanapa.vercel.app",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "",
+      video: "/vids/tanapa_demo.mp4",
     },
     {
       title: "llm.report",
@@ -294,7 +294,7 @@ export const DATA = {
         "Developed an AI Customer Support Chatbot which automatically responds to customer support tickets using the latest GPT models.",
       technologies: [
         "Next.js",
-        "Typescript",
+        "JavaScript",
         "PostgreSQL",
         "Prisma",
         "TailwindCSS",
